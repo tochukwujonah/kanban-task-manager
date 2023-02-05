23327a38851690556f2196c5598b4616617f4e57
Tochukwu Jonah <31604841+tochukwujonah@users.noreply.github.com>
2023-02-05 13:51:55 +0100

    Add tailwinfCSS and Set up routing

--- kanban-task-manager/src/main.jsx
+++ kanban-task-manager/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
 import './index.css'
+import Root from './routes/root'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import ComponentsContainer from './components/ComponentsCont'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/components",
+    element: <ComponentsContainer />,
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )
